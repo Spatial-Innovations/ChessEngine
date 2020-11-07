0b0f8c690915ad460939b1031934887d5948a991
Patrick <huangpatrick16777216@gmail.com>
2020-11-07 11:03:46 -0800

    Added printing.

--- _tree.py
+++ _tree.py
@@ -15,24 +15,54 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import time
 import threading
 from copy import deepcopy
 from chess import Board
 
 
 class Tree:
+    infoStr = "info depth {depth} seldepth {depth} multipv 1 score cp {cp} nodes {nodes} nps {nps} tbhits 0 time {time} pv {moves}"
+
     def __init__(self):
         self.processing = False
         self.nodes = 0
+        self.depth = 0
 
     def Go(self, **kwargs):
         threading.Thread(target=self.Stopper, args=()).start()
+        threading.Thread(target=self.Printer, args=()).start()
+
+        self.processing = True
+        self.nodes = 0
         self.board = kwargs["board"]
+        self.root = Node(self, self.board, 0)
+        self.timeStart = time.time()
         if "depth" in kwargs:
             pass
         else:
-            pass
+            for depth in range(10000):
+                self.depth = depth
+                self.root.Branch(depth)
 
+        self.processing = False
+
+    def Printer(self):
+        expBase = 25000   #* Print every time self.nodes reaches the next multiple of this number.
+        currExp = 0
+        print()
+        while True:
+            while self.nodes < expBase*currExp:
+                if not self.processing:
+                    return
+                time.sleep(0.01)
+
+            currExp += 1
+            timeElapse = time.time() - self.timeStart
+            string = self.infoStr.format(depth=self.depth, cp=0, nodes=self.nodes, nps=int(self.nodes/timeElapse), time=int(timeElapse*1000), moves=None)
+            print(string)
+
+    
     def Stopper(self):
         while True:
             if input().strip() == "stop":
@@ -41,11 +71,13 @@ class Tree:
 
 
 class Node:
-    def __init__(self, root, board: Board, depth):
-        self.root = root
+    def __init__(self, tree: Tree, board: Board, depth):
+        self.tree = tree
         self.board = board
         self.depth = depth
         self.branches = []
+
+        tree.nodes += 1
 
     def Branch(self, targetDepth):
         if targetDepth == self.depth + 1:
@@ -53,7 +85,7 @@ class Node:
             for move in self.board.generate_legal_moves():
                 newBoard = deepcopy(self.board)
                 newBoard.push(move)
-                newNode = (self.root, newBoard, newDepth)
+                newNode = Node(self.tree, newBoard, newDepth)
                 self.branches.append(newNode)
 
         elif targetDepth > self.depth + 1:
