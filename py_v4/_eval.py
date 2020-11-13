85161ed968a85ed36dd14faf50af227ed339f61a
Patrick <huangpatrick16777216@gmail.com>
2020-11-13 09:02:38 -0800

    Removed piece maps.

--- py_v4/_eval.py
+++ py_v4/_eval.py
@@ -20,16 +20,6 @@ from chess import Board
 from _maps import *
 from itertools import groupby
 
-# attacking, positional
-PERSONALITY = "normal"
-
-if PERSONALITY == "normal":
-    WEIGHTS = {"mat": 3, "center": 0.75, "pawn": 0.8, "pieceMap": 0.6}
-elif PERSONALITY == "attacking":
-    WEIGHTS = {"mat": 3, "center": 0.65, "pawn": 0.77, "pieceMap": 0.63}
-elif PERSONALITY == "positional":
-    WEIGHTS = {"mat": 3.2, "center": 0.6, "pawn": 0.82, "pieceMap": 0.57}
-
 
 def Eval(position: Board):
     if position.is_game_over():
@@ -41,52 +31,12 @@ def Eval(position: Board):
         elif result == "1/2-1/2":
             return 0
 
-    moveNum = len(position.move_stack)
+    material = Material(position)
+    center = CenterControl(position)
+    pawns = Pawns(position)
 
-    if PERSONALITY == "dynamic":
-        mat = Material(position) * max(min(0.03*moveNum + 1.7, 3), 2)
-        center = CenterControl(position) * max(min(-0.015*moveNum + 0.9, 0.7), 0.3)
-        pawn = PawnStruct(position) * max(min(-0.002*moveNum + 0.1, 0.1), 0.02)
-        pieceMap = Map(position, moveNum) * max(min(-0.002*moveNum + 0.1, 0.1), 0.02)
-    else:
-        mat = Material(position) * WEIGHTS["mat"]
-        center = CenterControl(position) * WEIGHTS["center"]
-        pawn = PawnStruct(position) * WEIGHTS["pawn"]
-        pieceMap = 0 if moveNum > 25 else Map(position, moveNum) * WEIGHTS["pieceMap"]
-
-    evaluation = mat + center + pawn + pieceMap
+    evaluation = material + pawns + center
     return int(evaluation * 20)
-
-
-def Map(position, moveNum):
-    points = 0
-    fen = position.fen().split(" ")[0].replace("/", "").replace("1", " ").replace("2", " "*2).replace("3", " "*3).replace("4", " "*4).replace("5", " "*5).replace("6", " "*6).replace("7", " "*7).replace("8", " "*8)
-
-    if moveNum <= 10:
-        for map, pieceType in zip((MAP_BEG_P, MAP_BEG_N, MAP_BEG_B, MAP_BEG_R, MAP_BEG_Q, MAP_BEG_K), ("p", "n", "b", "r", "q", "k")):
-            for i, piece in enumerate(fen):
-                row, col = i//8, i%8
-                if piece.lower() == pieceType:
-                    if piece.isupper():
-                        points += map[row][col]
-                    elif piece.islower():
-                        points -= map[7 - row][col]
-
-    elif 10 < moveNum <= 25:
-        for map, pieceType in zip((MAP_MID_P, MAP_MID_N, MAP_MID_B, MAP_MID_R, MAP_MID_Q, MAP_MID_K), ("p", "n", "b", "r", "q", "k")):
-            for i, piece in enumerate(fen):
-                row, col = i//8, i%8
-                if piece.lower() == pieceType:
-                    if piece.isupper():
-                        points += map[row][col]
-                    elif piece.islower():
-                        points -= map[7-row][col]
-
-    else:
-        points = 0
-
-    # Value around 1 pawn
-    return points / 100
 
 
 def Material(position: Board):
@@ -106,6 +56,7 @@ def CenterControl(position: Board):
     for sq in squares:
         inner += len(position.attackers(chess.WHITE, getattr(chess, sq)))
         inner -= len(position.attackers(chess.BLACK, getattr(chess, sq)))
+    inner /= len(squares)
 
     # Outer center, weighted 0.2
     outer = 0
@@ -113,12 +64,12 @@ def CenterControl(position: Board):
     for sq in squares:
         outer += len(position.attackers(chess.WHITE, getattr(chess, sq)))
         outer -= len(position.attackers(chess.BLACK, getattr(chess, sq)))
+    outer /= len(squares)
 
-    # Value around 1.5 pawns
-    return (inner + outer/5) / 30
+    return (inner + outer/5) / 3
 
 
-def PawnStruct(position: Board):
+def Pawns(position: Board):
     pawnsW = [(p//8, p%8) for p in position.pieces(chess.PAWN, True)]
     pawnsB = [(p//8, p%8) for p in position.pieces(chess.PAWN, False)]
 
@@ -192,6 +143,5 @@ def PawnStruct(position: Board):
         blackAvgRank /= len(pawnsB)
 
     # Final
-    score = 0.5*((4-whiteIslands)-(4-blackIslands)) + (whitePassed - blackPassed) - 0.1*(whiteStackScore-blackStackScore) + 0.5*(whiteAvgRank-blackAvgRank)
-    # Value around 0.8 pawns
-    return score / 20
+    score = 0.1*((4-whiteIslands)-(4-blackIslands)) + 0.3*(whitePassed - blackPassed) - 0.1*(whiteStackScore-blackStackScore) + 0.2*(whiteAvgRank-blackAvgRank)
+    return score
